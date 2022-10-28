cfe13f728c9df901d251590eb17e253267a0c0c4
GrettJ <greta.johannsen@gmail.com>
2022-10-28 18:49:30 +0200

    Control de entradas

--- Greta_Johannsen.py
+++ Greta_Johannsen.py
@@ -1,108 +1,87 @@
 from tabulate import tabulate
 
 
+def catcher(entrada):
+    """
+    Se encarga de dividir el tipo de entrada para procesar en la funcion process_matrix
+    """
+    if entrada == []:
+        return None
+    if type(entrada[0]) == int or type(entrada[0]) == float:
+        return "List"
+    elif type(entrada[0]) == list:
+        for x in entrada:
+            for y in x:
+                if type(y) != float and type(y) != int:
+                    return False
+                else:
+                    return "Matrix"
+    else:
+        return False
+
+
 def process_matrix(matriz):
     """
     Genera una matriz nueva con el numero promedio de la casilla y sus casillas vecinas
     para completa funcionalidad del programa es necesario instalar tabulate.
+    Trabaja junto a la funcion catcher.
     """
-    # creamos una matriz vacia donde guardamos los resultados
+    # Creamos una lista vacia donde guardamos los resultados
     Matrix = []
-    # Definimos los limites de la matriz
-    wide = len(matriz)
-    tall = len(matriz[0])
-
-    for x, column in enumerate(matriz):
-        # añadimos una lista vacia por cada columna
-        Matrix.append([])
-        for y, value in enumerate(column):
-            # Iniciamos el contador con el valor del elemento
-            suma = value
-            # creamos un contador para saber cuantos elementos sumamos y por cuanto hay que dividir
-            cont = 1
-            # en las variables guardamos a que casilla miramos.
-            up = y - 1
-            down = y + 1
-            left = x - 1
-            right = x + 1
-            # creamos 4 if ya que tiene que preguntar 4 veces si cumple la condicion y filtrar casos extremos
-            # dentro de cada if, si cumple la condicion sumamos el elemento de la casilla vecina y sumamos al contador
-            if up >= 0:
-                suma += matriz[x][up]
-                cont += 1
-            if down < tall:
-                suma += matriz[x][down]
-                cont += 1
-            if left >= 0:
-                suma += matriz[left][y]
-                cont += 1
-            if right < wide:
-                suma += matriz[right][y]
-                cont += 1
-            # despues de pasar todas las preguntas añado a la lista vacia
-            # en el mismo lugar del elemento inspeccionado
-            # recortamos cantidad de decimales para que quede bonito
-            Matrix[x].insert(y, round((suma/cont), 2))
-    # formateamos lista de listas para ver como tabla.
-    return tabulate(Matrix, tablefmt="plain")
-
-
-def process_matrix_lists(matriz):
-    """
-    Genera una matriz nueva con el numero promedio de la casilla y sus casillas vecinas
-    para completa funcionalidad del programa es necesario instalar tabulate.
-    """
-    # creamos una matriz vacia donde guardamos los resultados
-    Matrix = []
-    # Definimos los limites de la matriz
-    wide = len(matriz)
-    # generamos un primer if en caso de que nos pasen una lista
-    if type(matriz[0]) == int:
+    # llamamos al filtro. si devuelve que es una lista sencilla lo procesa de ese modo
+    if catcher(matriz) == "List":
+        wide = len(matriz)
         for x, value in enumerate(matriz):
             left = x - 1
             right = x + 1
-            suma = value
+            sum = value
             cont = 1
             if left >= 0:
-                suma += matriz[left]
+                sum += matriz[left]
                 cont += 1
             if right < wide:
-                suma += matriz[right]
+                sum += matriz[right]
                 cont += 1
-            Matrix.append(suma/cont)
-    else:
-        #continuamos el codigo para trabajar con lista de listas
+            Matrix.append(sum/cont)
+    elif catcher(matriz) == "Matrix":
+        # una vez confirmado que es una lista de listas definimos los limites
+        wide = len(matriz)
         tall = len(matriz[0])
-
-        for x, column in enumerate(matriz):
-            # añadimos una lista vacia por cada columna
+        for x, colum in enumerate(matriz):
+            # Añado una lista vacia para generar una matriz del mismo tamaño que la original
             Matrix.append([])
-            for y, value in enumerate(column):
-                # Iniciamos el contador con el valor del elemento
-                suma = value
-                # creamos un contador para saber cuantos elementos sumamos y por cuanto hay que dividir
+            for y, val in enumerate(colum):
+                # iniciamos el contador al valor del elemento
+                sum = val
                 cont = 1
-                # en las variables guardamos a que casilla miramos.
+                # en la variables guardamos la casilla que miramos
                 up = y - 1
                 down = y + 1
                 left = x - 1
                 right = x + 1
-                # creamos 4 if ya que tiene que preguntar 4 veces si cumple la condicion y filtrar casos extremos
+                # preguntamos 4 veces si cumple la condicion y filtrar casos extremos
                 # dentro de cada if, si cumple la condicion sumamos el elemento de la casilla vecina y sumamos al contador
                 if up >= 0:
-                    suma += matriz[x][up]
+                    sum += matriz[x][up]
                     cont += 1
                 if down < tall:
-                    suma += matriz[x][down]
+                    sum += matriz[x][down]
                     cont += 1
                 if left >= 0:
-                    suma += matriz[left][y]
+                    sum += matriz[left][y]
                     cont += 1
                 if right < wide:
-                    suma += matriz[right][y]
+                    sum += matriz[right][y]
                     cont += 1
                 # despues de pasar todas las preguntas añado a la lista vacia
                 # en el mismo lugar del elemento inspeccionado
-                # recortamos cantidad de decimales para que quede bonito
-                Matrix[x].insert(y, round((suma/cont), 2))
+                Matrix[x].insert(y, sum/cont)
+        # formateamos para ver el resultado como tabla
+        Matrix = tabulate(Matrix, tablefmt="plain")
+    elif catcher(matriz) == None:
+        # confima que sea una lista vacia, pasa para que se devuelva la lista vacia creada
+        pass
+    elif catcher(matriz) == False:
+        # devuelve un error, hay algun elemento en lo pasado que no es un numero
+        raise ValueError("Only numerical matrices")
     return Matrix
--- main.py
+++ main.py
@@ -1,27 +1,87 @@
-x = [[1,2,3,4], [1,2,0,0], [0,0,0,1], [1,2,3,4]]
-tmp = []
-ancho = len(x)
-alto = len(x[0])
+from tabulate import tabulate
 
-def funcion(matriz):
-    for x, colum in enumerate(matriz):
-        for y, val in enumerate(col):
-            media = 
-            up = index - 1
-            down = index + 1
-            left = lista[index-1] - 1
-            right = lista[index-1] + 1
-            if up >= 0:
-                tmp.append(matriz[index][up])
-            if down >= 0:
-                tmp.append(matriz[down][index])
-            if left < ancho:
-                tmp.append(matriz[left][index])
-            if right < alto:
-                tmp.append(matriz[right][index])
-            else:
-                tmp.append(i)     
-    return print(tmp)
 
-funcion(x)
+def catcher(entrada):
+    """
+    Se encarga de dividir el tipo de entrada para procesar en la funcion process_matrix
+    """
+    if entrada == []:
+        return None
+    if type(entrada[0]) == int or type(entrada[0]) == float:
+        return "List"
+    elif type(entrada[0]) == list:
+        for x in entrada:
+            for y in x:
+                if type(y) != float and type(y) != int:
+                    return False
+                else:
+                    return "Matrix"
+    else:
+        return False
 
+
+def process_matrix(matriz):
+    """
+    Genera una matriz nueva con el numero promedio de la casilla y sus casillas vecinas
+    para completa funcionalidad del programa es necesario instalar tabulate.
+    Trabaja junto a la funcion catcher.
+    """
+    # Creamos una lista vacia donde guardamos los resultados
+    Matrix = []
+    # llamamos al filtro. si devuelve que es una lista sencilla lo procesa de ese modo
+    if catcher(matriz) == "List":
+        wide = len(matriz)
+        for x, value in enumerate(matriz):
+            left = x - 1
+            right = x + 1
+            sum = value
+            cont = 1
+            if left >= 0:
+                sum += matriz[left]
+                cont += 1
+            if right < wide:
+                sum += matriz[right]
+                cont += 1
+            Matrix.append(sum/cont)
+    elif catcher(matriz) == "Matrix":
+        # una vez confirmado que es una lista de listas definimos los limites
+        wide = len(matriz)
+        tall = len(matriz[0])
+        for x, colum in enumerate(matriz):
+            # Añado una lista vacia para generar una matriz del mismo tamaño que la original
+            Matrix.append([])
+            for y, val in enumerate(colum):
+                # iniciamos el contador al valor del elemento
+                sum = val
+                cont = 1
+                # en la variables guardamos la casilla que miramos
+                up = y - 1
+                down = y + 1
+                left = x - 1
+                right = x + 1
+                # preguntamos 4 veces si cumple la condicion y filtrar casos extremos
+                # dentro de cada if, si cumple la condicion sumamos el elemento de la casilla vecina y sumamos al contador
+                if up >= 0:
+                    sum += matriz[x][up]
+                    cont += 1
+                if down < tall:
+                    sum += matriz[x][down]
+                    cont += 1
+                if left >= 0:
+                    sum += matriz[left][y]
+                    cont += 1
+                if right < wide:
+                    sum += matriz[right][y]
+                    cont += 1
+                # despues de pasar todas las preguntas añado a la lista vacia
+                # en el mismo lugar del elemento inspeccionado
+                Matrix[x].insert(y, sum/cont)
+        # formateamos para ver el resultado como tabla
+        Matrix = tabulate(Matrix, tablefmt="plain")
+    elif catcher(matriz) == None:
+        # confima que sea una lista vacia, pasa para que se devuelva la lista vacia creada
+        pass
+    elif catcher(matriz) == False:
+        # devuelve un error, hay algun elemento en lo pasado que no es un numero
+        raise ValueError("Only numerical matrices")
+    return Matrix
